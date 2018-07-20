62a665b8c806a59ee915cec01c2a6fa0faf9226d
Manuel Rodriguez <manrodri92@gmail.com>
2018-07-20 00:20:52 -0400

    Added french to Redux from scratch

--- reduxFromScratch/src/app/app.component.ts
+++ reduxFromScratch/src/app/app.component.ts
@@ -8,17 +8,19 @@ import { Store, Action} from './store.service';
 export class AppComponent {
 
   spanish;
+  french;
   constructor(private store:Store){
     this.spanish = store.select('spanish.hola');
+    this.french = store.select('french.bonjour');
   }
 
   set(){
     this.store.dispatch(new Action('SET', {hello:'world'}));
   }
   update(){
-    this.store.dispatch(new Action('UPDATE',{spanish:{hola:'mundo'}}));
+    this.store.dispatch(new Action('UPDATE',{spanish:{hola:'mundo'},french:{bonjour:'le monde'}}));
   }
   delete(){
-    this.store.dispatch(new Action('DELETE'));
+    this.store.dispatch(new Action('DELETE','spanish'));
   }
 }
