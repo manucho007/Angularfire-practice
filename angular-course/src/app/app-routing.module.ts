9a1107ca8b7dc8eb855842942ba6d9f4403c8b78
Manuel Rodriguez <manrodri92@gmail.com>
2020-06-29 20:16:05 +0300

    Added shared module and shell component

--- angular-course/src/app/app-routing.module.ts
+++ angular-course/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: HomePageComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
