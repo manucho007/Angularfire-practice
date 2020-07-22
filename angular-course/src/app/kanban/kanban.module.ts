958d44f4a7385ee9408311330ff6f4f9146fb6ef
Manuel Rodriguez <Manuel.Rodriguez@payever.org>
2020-07-23 01:51:31 +0300

    Added boards, oards lists and dialogs

--- angular-course/src/app/kanban/kanban.module.ts
+++ angular-course/src/app/kanban/kanban.module.ts
@@ -2,15 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { KanbanRoutingModule } from './kanban-routing.module';
-import { SharedModule } from '../shared/shared.module';
-import { FormsModule } from '@angular/forms';
-import { DragDropModule } from '@angular/cdk/drag-drop';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { BoardListComponent } from './board-list/board-list.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
+import { MatDialogModule } from '@angular/material/dialog';
+import { BoardComponent } from './board/board.component';
+import { FormsModule } from '@angular/forms';
+import { BoardDialogComponent } from './dialogs/board-dialog.component';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { TaskDialogComponent } from './dialogs/task-dialog.component';
 
 @NgModule({
-  declarations: [BoardListComponent],
+  declarations: [
+    BoardListComponent,
+    BoardComponent,
+    BoardDialogComponent,
+    TaskDialogComponent,
+  ],
   imports: [
     CommonModule,
     KanbanRoutingModule,
@@ -19,6 +28,8 @@ import { BoardListComponent } from './board-list/board-list.component';
     DragDropModule,
     MatDialogModule,
     MatButtonToggleModule,
+    RouterModule,
   ],
+  entryComponents: [BoardDialogComponent],
 })
 export class KanbanModule {}
